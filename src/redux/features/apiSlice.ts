b73a0510111b3ef3708d38389b6571430994f167
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-03-03 23:48:52 -0800

    Configured RTK Query

---
+++ src/redux/features/apiSlice.ts
@@ -0,0 +1,14 @@
+// Import the RTK Query methods from the React-specific entry point
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+
+export const apiSlice = createApi({
+  reducerPath: 'api',
+  baseQuery: fetchBaseQuery({ baseUrl: '/' }),
+  endpoints: builder => ({
+    getPosts: builder.query({
+      query: () => '/',
+    }),
+  }),
+});
+
+export const { useGetPostsQuery } = apiSlice;
