b831850214c9f95f3ac7b89fb50b0221457a08d2
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-01 12:16:43 -0700

    Created DropDown component

---
+++ src/components/Header/ProductNavBar/AuthContainer/components/DropDown/DropDown.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Route } from '@/constants/routes';
+import Link from 'next/link';
+import styles from './DropDown.module.css';
+
+interface DropDownProps {
+  locale: string | undefined;
+}
+
+const DropDown: React.FC<DropDownProps> = ({ locale }) => (
+  <div className={styles.dropDown}>
+    <span />
+    <ul>
+      <li>
+        <Link href='/'>Log In</Link>
+      </li>
+      <li>
+        <Link href={`/${locale}${Route.SIGN_UP}`}>Sign Up</Link>
+      </li>
+    </ul>
+  </div>
+);
+  
+export default DropDown;
