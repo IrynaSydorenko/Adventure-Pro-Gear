859b303968c8433510ff07d1220487867f7461a3
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-01 12:45:15 -0700

    Modified page using session

--- src/app/(main)/[lang]/page.tsx
+++ src/app/(main)/[lang]/page.tsx
@@ -1,6 +1,27 @@
 import React from 'react';
+import { options } from '@/config';
+import { getServerSession } from 'next-auth/next';
 import Hiro from '@/components/Hero/Hero';
+import AuthModal from '@/components/AuthModal';
 
-const Page = () => <Hiro />;
+const Page = async ({ params }: { params: { lang: string } }) => {
+  const session = await getServerSession(options);
+  return (
+    <>
+      {session ? (
+        <>
+          <Hiro />
+          <h2>Authenticated</h2>
+        </>
+      ) : (
+        <>
+          <Hiro />
+          <h2>Not Authenticated</h2>
+        </>
+      )}
+      <AuthModal />
+    </>
+  );
+};
 
 export default Page;
