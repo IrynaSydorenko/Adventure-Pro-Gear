514e8e1d0ae01dad4e282765b61671bdfb796d58
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-01 12:40:03 -0700

    Created Form reusable component

---
+++ src/components/Form/index.tsx
@@ -0,0 +1,34 @@
+// Form.tsx
+import React, { FormEvent } from 'react';
+import clsx from 'clsx';
+import styles from './Form.module.css';
+
+interface FormProps {
+  children: React.ReactNode;
+  // onSubmit: (event: FormEvent<HTMLFormElement>) => void;
+  action: (formData: FormData) => void;
+  className?: string;
+  // method?: 'GET' | 'POST'
+}
+
+const Form: React.FC<FormProps> = ({ 
+  children, 
+  className, 
+  // onSubmit, 
+  action, 
+  // method = 'POST' 
+}) => {
+  const formClassNames = clsx(styles.authForm, className);
+  return (
+    <form 
+      className={formClassNames} 
+      // onSubmit={onSubmit} 
+      action={action} 
+      // method={method}
+    >
+      {children}
+    </form>
+  );
+};
+
+export default Form;
