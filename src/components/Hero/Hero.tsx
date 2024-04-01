76059dae7ee9cde09b1b89a4e0d5c42a5736460b
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-01 12:22:10 -0700

    Modified Hero component: got products and users, and display it on the ui

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,31 @@
 import React from 'react';
+import { getProducts, getUsers, deletePost } from '@/services/axios';
 
-const Hero = () => <div>Hero!</div>;
+const Hero = async () => {
+  const { data } = await getProducts();
+  const { data: users } = await getUsers();
+
+  const handleDeletePost = async (postId: string) => {
+    try {
+      await deletePost(postId);
+    } catch (error) {
+    console.error('Error deleting post:', error);
+    }
+  };
+
+  return (
+    <div>
+      Hero!
+      <ul>
+        {/* @ts-ignore */}
+        {data && data.map((product: any) => (
+          <li key={product.id}>{product?.productName}</li>
+        ))}
+      </ul>
+      {/* @ts-ignore */}
+      <ul>{users && users.map((user, index) => <li key={user.id}>{user?.name}</li>)}</ul>
+    </div>
+  );
+};
 
 export default Hero;
