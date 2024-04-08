e3fce445a4380e2ef54504b322dd2fb5c2891a8f
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-08 08:53:32 -0700

    Modified form props

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -5,26 +5,26 @@ import styles from './Form.module.css';
 
 interface FormProps {
   children: React.ReactNode;
-  // onSubmit: (event: FormEvent<HTMLFormElement>) => void;
-  action: (formData: FormData) => void;
+  onSubmit?: (event: FormEvent<HTMLFormElement>) => void;
+  action?: ((formData: FormData) => void) | string;
   className?: string;
-  // method?: 'GET' | 'POST'
+  method?: 'GET' | 'POST'
 }
 
 const Form: React.FC<FormProps> = ({ 
   children, 
   className, 
-  // onSubmit, 
+  onSubmit, 
   action, 
-  // method = 'POST' 
+  method
 }) => {
   const formClassNames = clsx(styles.authForm, className);
   return (
     <form 
       className={formClassNames} 
-      // onSubmit={onSubmit} 
+      onSubmit={onSubmit} 
       action={action} 
-      // method={method}
+      method={method}
     >
       {children}
     </form>
