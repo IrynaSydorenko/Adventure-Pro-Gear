79936b58959bbe6baa9714f753e53a4e7590f59b
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-08 08:41:28 -0700

    Modified axios, added services for signIn, getUser, updated url for getUsers

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,39 +1,28 @@
 import axios from 'axios';
+import { getServerSession } from 'next-auth';
+import {options} from '@/config'
 
 const axiosInstance = axios.create({
-  // Customize other axios options as needed
-  baseURL: 'https://towering-house-production.up.railway.app',
-  // maxRedirects: 0, // Prevent axios from automatically following redirects
+  baseURL: 'https://prime-tax-production.up.railway.app/',
 });
 
-axiosInstance.interceptors.response.use(
-  (response) => {
-    // If the response is a redirect (status code 3xx)
-    if (response.status === 301 || response.status === 302 || response.status === 307 || response.status === 308) {
-      // Manually make a new request to the redirect URL
-      console.log('Redirected URL:', response.headers.location);
-      console.log('Response status code:', response.status);
-      return axiosInstance({
-        method: response.config.method, // Use the original HTTP method
-        url: response.headers.location, // Redirect URL
-        // You may need to include other request configuration options like data, params, headers, etc.
-        // For example:
-        // data: response.config.data,
-        // params: response.config.params,
-        // headers: response.config.headers,
-      });
-    }
-    // For non-redirect responses, just return the response
-    return response;
-  },
-  (error) => {
-    // Handle errors here
-    return Promise.reject(error);
-  }
-);
+// axiosInstance.interceptors.request.use(
+//   async config => {
+//     const session = await getServerSession(options);
+
+//     if (!config.headers.Authorization) {
+//       config.headers.Authorization = `Bearer ${session?.user?.token.access}`;
+//     }
+
+//     return config;
+//   },
+//   error => Promise.reject(error)
+// )
+
+// const session = getServerSession(options);
 
 export const getProducts = async () => {
-  return {data:undefined}; return await axiosInstance.get('/api/v1/products');
+  /*return {data:undefined};*/ return await axiosInstance.get('/api/v1/products');
 };
 
 export const signUpService = async (credentials: any) => {
@@ -49,12 +38,20 @@ export const signUpService = async (credentials: any) => {
   );
 };
 
+export const signInService = async (credentials: any) => {
+  return await axiosInstance.post('api/public/auth/signin', credentials)
+}
+
+export const getUserInfoService = async () => {
+   return await axiosInstance.get('/api/users/me')
+}
+
 export const getUsers = async () => {
-  return {data:undefined}; await axiosInstance.get('/api/users');
+  /*return {data:undefined};*/ await axiosInstance.get('/api/users');
 };
 
 export const deletePost = async (id: string) => {
-  return {data:undefined}; return await axiosInstance.delete(
+  /*return {data:undefined};*/ return await axiosInstance.delete(
     `/api/v1/products/${id}`
   );
 };
