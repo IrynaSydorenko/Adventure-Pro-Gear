fbbd0ae4ba71af48a27b6595306ca03555a1310a
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-08 08:51:08 -0700

    Added server action to submit the form, renamed the component

--- src/components/Header/ProductNavBar/AuthContainer/components/SignUp/index.tsx
+++ src/components/Header/ProductNavBar/AuthContainer/components/SignUp/index.tsx
@@ -1,13 +1,17 @@
 'use client';
 
 import React, { FormEvent, useState } from 'react';
-import { useRouter} from 'next/navigation';
+import { useRouter, usePathname } from 'next/navigation';
 import Form from '@/components/Form';
-import TextInput from '@/components/Input';
-import { register } from '@/app/actions'
+import Input from '@/components/Input';
+import Checkbox from '@/components/Checkbox/Checkbox';
+import { registerAction } from '@/app/actions';
+import {getAllTranslations, getTranslation} from '@/dictionaries/dictionaries';
+import {i18n, Locale} from '@/i18n-config';
 import { Route } from '@/constants/routes';
 import styles from './SignUp.module.css';
 import { signUpService } from '@/services/axios';
+import Link from 'next/link';
 
 export interface Credentials {
   name: string;
@@ -23,31 +27,54 @@ function SignUp() {
     email: '',
     password: '',
   });
+
   const router = useRouter();
+
+  // const getLocale = () => {
+  //   const pathName = usePathname();
+  //   const segmentsArray = pathName.split('/');
+  //   const locale = segmentsArray.find(segment => segment === (i18n.locales[0] || i18n.locales[1]))
+  //   console.log(locale);
+  //   return locale;
+
+  // }
+  
+  // const locale = getLocale() as Locale;
+  // console.log(locale);
+
+  // const getTranslationOfRequest = () => {
+  //   if(locale){
+  //     const translationJsonFile = getAllTranslations(locale);
+  //     const translation = getTranslation(translationJsonFile);
+  //     const submitPolicy = translation('auth');
+  //     console.log(submitPolicy)
+  //   }
+    
+  // };
+  // getTranslationOfRequest();
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setCredentials({ ...credentials, [event.target.name]: event.target.value });
   };
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    try {
-      await signUpService(credentials);
-      router.push(Route.HOME);
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
-  };
+  // const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  //   event.preventDefault();
+  //   try {
+  //     await signUpService(credentials);
+  //     router.push(Route.HOME);
+  //   } catch (error) {
+  //     console.error('Error submitting form:', error);
+  //   }
+  // };
 
   return (
     <div className={styles.formContainer}>
       <Form
         className={styles.form}
-        action={register}
-        // onSubmit={handleSubmit}
+        action={registerAction}
       >
         <h4 className={styles.h4}>Registration</h4>
-        <TextInput
+        <Input
           type='text'
           value={credentials.name}
           onChange={handleChange}
@@ -55,7 +82,7 @@ function SignUp() {
           placeholder="Name"
           error='incorrect name'
         />
-        <TextInput
+        <Input
           type='text'
           name='surname'
           value={credentials.surname}
@@ -63,7 +90,7 @@ function SignUp() {
           placeholder="Surname"
           error='Incorrect surname'
         />
-        <TextInput
+        <Input
           type='email'
           name='email'
           value={credentials.email}
@@ -71,7 +98,7 @@ function SignUp() {
           placeholder="Email"
           error='incorrect imail'
         />
-        <TextInput
+        <Input
           type='password'
           name='password'
           value={credentials.password}
@@ -79,8 +106,10 @@ function SignUp() {
           placeholder="Password"
           error='incorrect password'
         />
-        <input type="submit" value="Register" />
-        {/* <Button text={'Register'} onClick={handleClick} type="submit" /> */}
+        <Checkbox/>
+        <div className={styles.submitPolicy}>Реєструючись, ви погоджуєтеся з умовами<Link href="/policy"> положення про обробку і захист персональних даних та угодою користувача</Link></div>
+        <Input type="submit" value="Register" />
+        <div>Я вже зареєстрований</div>
       </Form>
     </div>
   );
