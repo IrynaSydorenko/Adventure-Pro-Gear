aa4b246d6fa73815f3ad035bce33078b80c25976
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-08 08:47:58 -0700

    Passed locale to modal

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -3,13 +3,14 @@
 import React from 'react';
 import styles from './Modal.module.css';
 
-interface ModalProps {
+interface ModalProps extends React.HTMLAttributes<HTMLDivElement>{
   children: React.ReactNode;
+  locale: string;
   closeModal: () => void;
   className?: string;
 }
 
-const Modal: React.FC<ModalProps> = ({ children, className, closeModal }) => (
+const Modal: React.FC<ModalProps> = ({ children, className, closeModal, locale }) => (
   <div className={`${className} ${styles.modal}`}>
     <div>{children}</div>
     <button onClick={closeModal} className={styles.closeButton} type='button'>
