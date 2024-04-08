3fd6d2efee9ae518f7f300ec8e7bf7da837e7ba2
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-08 08:43:54 -0700

    Createdd file to avoid ts errors

---
+++ src/interfaces/next-auth.d.ts
@@ -0,0 +1,18 @@
+import NextAuth from 'next-auth/next';
+import { DefaultSession } from 'next-auth';
+
+declare module 'next-auth'{
+  interface Session {
+    user: {
+      name: string,
+      surname: string,
+      role?: 'admin' | 'user' | 'guest';
+    } & DefaultSession['guest'];
+  }
+}
+
+declare module 'next-auth/jwt' {
+  interface JWT {
+    role: 'admin' | 'user' | 'guest';
+  }
+}
