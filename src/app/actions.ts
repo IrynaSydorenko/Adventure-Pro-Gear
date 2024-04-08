e3748ea200647f57644887f81c348f8b3da4db40
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-08 09:00:54 -0700

    Modified actions

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,24 +1,32 @@
 'use server'
 
-import {signUpService} from '@/services/axios'
+import { signIn } from 'next-auth/react';
+import { signUpService } from '@/services/axios'
 
-export const register = async (formData: FormData) => {
-  console.log(formData);
-  // const name = formData.get('inputName');
-  // const surname = formData.get('inputSurname');
-  // const email = formData.get('inputEmail');
-  // const password = formData.get('inputPassword');
+export const registerAction = async (formData: FormData) => {
+  // console.log(formData);
   const credentials = Object.fromEntries(formData)
   console.log('CREDENTIALS:', credentials);
-  
-
   try {
         const result = await signUpService(credentials);
+        // const result2 = await signIn('credentials', { redirect: false, credentials });
+
         console.log('Form submitted successfully:', result.data);
         // router.push(Route.HOME);
       } catch (error) {
         console.error('Error submitting form:', error);
       }
-
-
 }
+
+// export const signInAction = async (formData: FormData) => {
+//   // console.log(formData);
+//   const signInCredentials = Object.fromEntries(formData)
+//   console.log('SignInCredentials: ', signInCredentials);
+//   try {
+//     const result = await signIn('credentials', { redirect: false, ...signInCredentials });
+//     console.log('Form submitted successfully:', result);
+//     // router.push(Route.HOME);
+//   } catch (error) {
+//     console.error('Error submitting form:', error);
+//   }
+// }
