42dca5bd2de418ef68f95bfa23bcb122a4f642de
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-04-24 16:12:40 -0700

    Modified Button component, added an ability to change the collour of the button and a size via passing props to it. Added styles for the button.

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -8,18 +8,17 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   text: string;
   onClick?: (event: any) => void;
   className?: string;
+  type?: "button" | "submit" | "reset" | undefined,
+  color?: string;
   icon?: React.ReactNode;
 }
 
-const Button: React.FC<ButtonProps> = ({ onClick, text, className = '', icon, ...props }) => {
-  const buttonClassNames = clsx(styles.button, className, {
-    // [styles.disabled]: disabled,
-    // [styles.expanded]: expanded
-  });
+const Button: React.FC<ButtonProps> = ({ onClick, text, className = '', icon, color='primary', type='button', ...props }) => {
+  const buttonClassNames = clsx(styles.button, className, styles[color]);
 
   return (
     <div>
-      <button className={buttonClassNames} onClick={onClick} {...props}>
+      <button className={buttonClassNames} onClick={onClick} type={type} {...props}>
         {text}
         {icon}
       </button>
