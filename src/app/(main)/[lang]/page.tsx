19a02f55063a15aebce1311ff4802578650f1460
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-05-09 10:29:10 -0700

    eslint fixes

--- src/app/(main)/[lang]/page.tsx
+++ src/app/(main)/[lang]/page.tsx
@@ -7,14 +7,14 @@ import AuthModal from '@/components/AuthModal';
 
 const Page = async ({ params }: { params: { lang: string } }) => {
   const session = await getServerSession(options);
-  console.log("session: ", session)
+  console.log('session: ', session);
   return (
     <>
       {session ? (
         <>
           <Hiro />
           <h2>Authenticated</h2>
-          <SignOutButton/>
+          <SignOutButton />
         </>
       ) : (
         <>
@@ -22,7 +22,7 @@ const Page = async ({ params }: { params: { lang: string } }) => {
           <h2>Not Authenticated</h2>
         </>
       )}
-      <AuthModal locale={params.lang}/>
+      <AuthModal locale={params.lang} />
     </>
   );
 };
