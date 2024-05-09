6bf2da2fbd3f0dd576b4cd568812003e6a35f61a
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-05-09 10:26:07 -0700

    lint fixes

--- src/components/AuthProvider.tsx
+++ src/components/AuthProvider.tsx
@@ -2,8 +2,8 @@
 
 import { SessionProvider } from 'next-auth/react';
 
-const AuthProvider = ({children}: {children: React.ReactNode}) => {
-  return <SessionProvider>{children}</SessionProvider>
-}
+const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  return <SessionProvider>{children}</SessionProvider>;
+};
 
 export default AuthProvider;
