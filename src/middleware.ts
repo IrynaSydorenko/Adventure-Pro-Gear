57fdc1ac3d2edbd5c1be2ef72dfa614d88f05f40
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-05-09 10:28:42 -0700

    filtered routes for user role and refactored routing a bit

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,28 +2,47 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 import { match as matchLocale } from '@formatjs/intl-localematcher';
 import Negotiator from 'negotiator';
-import { Routes } from '@/constants/routes'
-import { i18n } from './i18n-config';
+import { AppRoutes } from '@/constants/routes';
 import { getSession, GetSessionParams } from 'next-auth/react';
+import { i18n } from './i18n-config';
 
+console.log('Middleware is called!');
+const publicRoutes = [
+  `${AppRoutes.HOME}`,
+  `${AppRoutes.SIGNIN}`,
+  `${AppRoutes.SIGN_UP}`,
+  '/contacts/',
+  '/about_us/',
+  '/policy/',
+];
 
-const publicRoutes = [`${Routes.HOME}`, `${Routes.SIGNIN}`, `${Routes.SIGN_UP}`, '/contacts/', '/about_us/'];
-const protectedUserRoutes = [...publicRoutes, '/user', '/blog/'];
-const protectedAdminRoutes = ['/admin'];
+const filterUserRoutes = (arrayOfRoutes: string[]) => {
+  return arrayOfRoutes.filter(
+    route => route !== `${AppRoutes.SIGNIN}` && route !== `${AppRoutes.SIGN_UP}`
+  );
+};
+
+const filteredPublicRoutes = filterUserRoutes(publicRoutes);
+
+console.log('filteredPublicRoutes: ', filteredPublicRoutes);
+
+const protectedUserRoutes = [...filteredPublicRoutes, '/user', '/blog/', '/personal_account/'];
+const protectedAdminRoutes = [...protectedUserRoutes, '/admin'];
 
 const convertToGetSessionParams = (req: NextRequest): GetSessionParams => ({
   req: {
-      headers: {
-          cookie: req.headers.get('cookie') ?? undefined
-      }
-  }
+    headers: {
+      cookie: req.headers.get('cookie') ?? undefined,
+    },
+  },
 });
 
 export const getLocale = (request: NextRequest): string | undefined => {
+  console.log('This function is called!');
   // Negotiator expects plain object so we need to transform headers
   const negotiatorHeaders: Record<string, string> = {};
   request.headers.forEach((value, key) => (negotiatorHeaders[key] = value));
-  console.log(negotiatorHeaders);
+  console.log('negotiator headers: ', negotiatorHeaders);
   // @ts-ignore locales are readonly
   const locales: string[] = i18n.locales;
   // Use negotiator and intl-localematcher to get best locale
@@ -35,57 +54,62 @@ export const getLocale = (request: NextRequest): string | undefined => {
 export function hasLocale(pathname: string) {
   return i18n.locales.some(locale => pathname.startsWith(`/${locale}/`));
 }
-  
-  
+
 export async function getSessionAndRole(req: NextRequest) {
   const session = await getSession(convertToGetSessionParams(req));
   return session?.user?.role || 'guest';
 }
 
-
 export function checkAccess(role: string, pathname: string) {
   if (role === 'ADMIN') return true;
   if (role === 'USER' && protectedUserRoutes.includes(pathname)) return true;
   if (publicRoutes.includes(pathname)) {
-    console.log("Includes: ", publicRoutes.includes(pathname))
-    return true
-  };
+    console.log('Includes: ', publicRoutes.includes(pathname));
+    return true;
+  }
   return false;
 }
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
-  console.log("PATH_NAME: ", pathname) // PATH_NAME:  /uk-UA/contacts/;
+  const pathNameWithQueryParams = req.url;
+  console.log('pathNameWithQueryParams: ', pathNameWithQueryParams);
+  console.log('PATH_NAME: ', pathname); // PATH_NAME:  /uk-UA/contacts/;
 
   // Check access rights first
   const role = await getSessionAndRole(req);
-  console.log('ROLE: ', role)
+  console.log('ROLE: ', role);
   const isLocale = hasLocale(pathname);
+  console.log('Has locale: ', isLocale);
   let parts = pathname.split('/'); // [ '', 'uk-UA', 'contacts', '' ]
-  console.log(parts)
-  const locale = parts.splice(1, 1); 
-  let newPathname = parts.join('/'); 
-  newPathname = newPathname === "//" ? "/" : newPathname;
-  console.log(newPathname);
+  console.log(parts);
+  const locale = parts.splice(1, 1)[0];
+  let newPathname = parts.join('/');
+  newPathname = newPathname === '//' ? '/' : newPathname;
+  console.log('NewPathName', newPathname);
   const pathnameCleaned = isLocale ? newPathname : pathname;
 
   const isAllowed = checkAccess(role, pathnameCleaned);
-  console.log("Is allowed: ", isAllowed)
+  console.log('Is allowed: ', isAllowed);
 
   // http://localhost:3000/uk-UA/?auth=signin
   // http://localhost:3000/uk-UA/?auth=signin
 
   if (!isAllowed && isLocale) {
-    const targetUrl = role === 'guest' ? ('/' + locale + Routes.SIGNIN) :  ('/' + locale + '/error');
+    const targetUrl = role === 'guest' ? `/${locale}${AppRoutes.SIGNIN}` : `/${locale}/error`;
     return NextResponse.redirect(new URL(targetUrl, req.url));
   } else if (!isAllowed) {
-    const targetUrl = role === 'guest' ? ('/' + i18n.defaultLocale + Routes.SIGNIN) :  ('/' + i18n.defaultLocale + '/error');
+    const targetUrl =
+      role === 'guest'
+        ? `/${i18n.defaultLocale}${AppRoutes.SIGNIN}`
+        : `/${i18n.defaultLocale}/error`;
+
     return NextResponse.redirect(new URL(targetUrl, req.url));
   }
 
   // Then handle locale redirection for authorized users
   if (!isLocale) {
-    return NextResponse.redirect(new URL(`/${i18n.defaultLocale}${pathname}`, req.url));
+    return NextResponse.redirect(new URL(`/${i18n.defaultLocale}/${pathname}`, req.url));
   }
 
   return NextResponse.next();
@@ -95,109 +119,3 @@ export const config = {
   // Matcher ignoring `/_next/` and `/api/`
   matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)'],
 };
-
-
-
-
-
-
-
-
-
-
-
-
-
-// export const config = {
-//   matcher: ['/:path*'],
-// };
-
-
-
-// const getUserStatus = (role: string | null) => {
-//   if (role === 'ADMIN') {
-//     return 'admin'
-//   } else if (role === "USER") {
-//     return 'user'
-//   } else {
-//     return 'guest'
-//   }
-// }
-
-// const getRequiredStatus = (pathname: string) => {
-//   if (protectedAdminRoutes.includes(pathname)) {
-//     return 'admin'
-//   } else if (protectedUserRoutes.includes(pathname)) {
-//     return 'user'
-//   } else {
-//     console.log('PATH_NAME: ', pathname)
-//     return 'guest'
-//   }
-// }
-
-// export const middleware = async (req: NextRequest) => {
-//   const pathname = req.nextUrl.pathname;
-//   console.log('REQUEST: ', req);
-//   // const token = await getToken( req );
-//   // console.log('Token from middleware: ', token)
-//   const session = await getSession(convertToGetSessionParams(req));
-//   console.log('Session from request: ', session)
-//   const userStatus = getUserStatus(session?.user?.role);
-//   console.log('UserStatus: ', userStatus)
-//   const requiredStatus = getRequiredStatus(req.nextUrl.pathname);
-//   console.log("Required status: ", requiredStatus)
-
-//   // if (userStatus !== requiredStatus) {
-//   //   if (userStatus === 'guest' || userStatus === 'user' || userStatus === 'admin') {
-//   //     return NextResponse.redirect('http://localhost:3000/uk-UA/')
-//   //   } else {
-//   //     return NextResponse.redirect('http://localhost:3000/uk-UA/error')
-//   //   }
-//   // }
-
-//   // Check if there is any supported locale in the pathname
-//   const pathnameIsMissingLocale = i18n.locales.every(
-//     locale => !pathname.startsWith(`/${locale}/`) && pathname !== `/${locale}`
-//   );
-
-//   // Redirect if there is no locale
-//   if (pathnameIsMissingLocale) {
-//     const locale = getLocale(req);
-
-//     // e.g. incoming request is /products
-//     // The new URL is now /en-US/products
-//     if (locale) {
-//       // Construct redirect URL with the determined locale
-//       return NextResponse.redirect(
-//         new URL(`/${locale}${pathname.startsWith('/') ? '' : '/'}${pathname}`, req.url)
-//       );
-//     } else {
-//       // Handle case where no suitable locale could be determined
-//       // For example, redirect to a default locale or display an error page
-//       return NextResponse.redirect(new URL(`/${i18n.defaultLocale}${pathname}`, req.url));
-//     }
-//   }
-// };
-
-// export const getRole = async (req: NextRequest) => {
-//   const pathname = req.nextUrl.pathname;
-//   console.log('REQUEST: ', req);
-//   // const token = await getToken( req );
-//   // console.log('Token from middleware: ', token)
-//   const session = await getSession(convertToGetSessionParams(req));
-//   console.log('Session from request: ', session)
-//   const userStatus = getUserStatus(session?.user?.role);
-//   console.log('UserStatus: ', userStatus)
-//   const requiredStatus = getRequiredStatus(pathname);
-//   console.log("Required status: ", requiredStatus)
-
-//   if (userStatus !== requiredStatus) {
-//     if (userStatus === 'guest') {
-//       return NextResponse.redirect('http://localhost:3000/')
-//     } else {
-//       return NextResponse.redirect('http://localhost:3000/error')
-//     }
-//   }
-// }
-
-
