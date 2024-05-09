e961c63e0a64e97804df5a95cbdaed9b0ab4db9c
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-05-09 10:08:38 -0700

    temporary comented receiving products and displaying them on the main page

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import { getProducts, getUsers, deletePost } from '@/services/axios';
+import styles from './Hero.module.css';
 
 const Hero = async () => {
-  const { data } = await getProducts();
+  // const { data } = await getProducts();
   // console.log(data);
   // const users = await getUsers();
 
@@ -10,18 +11,18 @@ const Hero = async () => {
     try {
       await deletePost(postId);
     } catch (error) {
-    console.error('Error deleting post:', error);
+      console.error('Error deleting post:', error);
     }
   };
 
   return (
-    <div>
+    <div className={styles.hero}>
       Hero!
       <ul>
         {/* @ts-ignore */}
-        {data && data.map((product: any) => (
+        {/* {data && data.map((product: any) => (
           <li key={product.id}>{product?.productName}</li>
-        ))}
+        ))} */}
       </ul>
       {/* @ts-ignore */}
       {/* <ul>{users && users.map((user, index) => <li key={user.id}>{user?.name}</li>)}</ul> */}
