5e7456f0e86f34b1aa8bda15e2ae49eb996ade6d
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-06-26 19:29:46 -0700

    fixed the type for locale and added ProfileMenu component with logic

--- src/components/Header/ProductNavBar/AuthContainer/components/DropDown/DropDown.tsx
+++ src/components/Header/ProductNavBar/AuthContainer/components/DropDown/DropDown.tsx
@@ -1,33 +1,63 @@
 'use client';
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { AppRoutes } from '@/constants/routes';
+import { Locale } from '@/i18n-config';
+import { getAllTranslations, getTranslation } from '@/dictionaries/dictionaries';
+import { useSession } from 'next-auth/react';
+import ProfileMenu from '@/components/ProfileMenu';
 import Link from 'next/link';
 import styles from './DropDown.module.css';
 
 interface DropDownProps {
-  locale: string | undefined;
+  locale: Locale;
   isLinkClicked: () => void;
   personalAccount?: string;
   className?: string;
 }
 
-const DropDown: React.FC<DropDownProps> = ({ locale, className, isLinkClicked }) => (
-  <div className={` ${className} ${styles.dropDown}`}>
-    <span />
-    <ul>
-      <li>
-        <Link href={`/${locale}${AppRoutes.SIGNIN}`} onClick={isLinkClicked}>
-          Log In
-        </Link>
-      </li>
-      <li>
-        <Link href={`/${locale}${AppRoutes.SIGN_UP}`} onClick={isLinkClicked}>
-          Sign Up
-        </Link>
-      </li>
-    </ul>
-  </div>
-);
+interface ProfileTranslations {
+  menuData: string[];
+}
+
+const DropDown: React.FC<DropDownProps> = ({ locale, className, isLinkClicked }) => {
+  const session = useSession();
+  const [menuDataTranslation, setMenuDataTranslation] = useState<ProfileTranslations | undefined>();
+  console.log(session);
+  useEffect(() => {
+    const loadTranslations = async () => {
+      const translations = await getAllTranslations(locale);
+      const translationFunction = getTranslation(translations);
+      setMenuDataTranslation(translationFunction('profile'));
+    };
+
+    loadTranslations();
+  }, [locale]);
+  return (
+    <div className={`${className} ${styles.dropDown}`}>
+      {session.data ? <span className={styles.dropdownSpan} /> : <span />}
+      {session.data ? (
+        <ProfileMenu
+          menuData={menuDataTranslation && menuDataTranslation.menuData}
+          className={styles.profileMenu}
+          locale={locale}
+        />
+      ) : (
+        <ul>
+          <li>
+            <Link href={`/${locale}${AppRoutes.SIGNIN}`} onClick={isLinkClicked}>
+              Log In
+            </Link>
+          </li>
+          <li>
+            <Link href={`/${locale}${AppRoutes.SIGN_UP}`} onClick={isLinkClicked}>
+              Sign Up
+            </Link>
+          </li>
+        </ul>
+      )}
+    </div>
+  );
+};
 
 export default DropDown;
