6cfb9d670acd9149358a6967c0fe84a1b9678ef6
IrynaSydorenko <iryna.sydorenko1998@gmail.com>
2024-08-02 12:47:54 -0700

    fix: Fixed receiving products on a page

--- src/app/(main)/[lang]/page.tsx
+++ src/app/(main)/[lang]/page.tsx
@@ -3,24 +3,27 @@ import { options } from '@/config';
 import { Locale } from '@/i18n-config';
 import Container from '@/components/Container';
 import { getServerSession } from 'next-auth/next';
-import Hiro from '@/components/Hero/Hero';
+import { getProducts } from '@/services/axios';
+import Hero from '@/components/Hero/Hero';
 import SignOutButton from '@/components/SignOutButton';
 import AuthModal from '@/components/AuthModal';
 
 const Page = async ({ params }: { params: { lang: Locale } }) => {
   const session = await getServerSession(options);
   console.log('session: ', session);
+  const { data } = await getProducts();
+  console.log('Data: ', data);
   return (
     <>
       {session ? (
         <>
-          <Hiro />
+          <Hero data={data} />
           <h2>Authenticated</h2>
           <SignOutButton />
         </>
       ) : (
         <>
-          <Hiro />
+          <Hero data={data} />
           <h2>Not Authenticated</h2>
         </>
       )}
