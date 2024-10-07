c81f6848a6031c7531ed8335d33b8fe95f7215b9
unknown <kvalentyna@hotmail.com>
2024-10-07 14:44:53 +0300

    chore: change breakpoints

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,15 +1,8 @@
 'use client'
-import React, { useState } from 'react';
-import QuantitySelector from '../QuantitySelector/QuantitySelector';
 
 const Footer = () => {
-    const [quantity, setValue] = useState(1)
-    const handleChanged = (value: number) => {
-        console.log(' from footer', value)
-        setValue(value)
-    }
-    return <div style={{ padding: 20 }}><QuantitySelector onChange={handleChanged} quantity={10} variant='standart' />
-        <h3>{quantity}</h3></div>;
+    
+    return <>Footer</>>
 }
 
 export default Footer;
--- src/components/TempComponent/index.tsx
+++ src/components/TempComponent/index.tsx
@@ -1,30 +1,51 @@
-'use client'
+'use client';
+
 import { Locale } from '@/i18n-config';
-import Card from "../Card"
+import Card from '../Card';
 
 interface TempProps {
-    product: any;
-    locale: Locale;
-    avgRating?: number;
-    countReviews?: number;
-    translation: {
-        card: {
-            addToFollowing: string;
-            sale: string;
-            new: string;
-            available: string;
-            outOfStock: string;
-            buy: string;
-        }
+  product: any;
+  locale: Locale;
+  avgRating?: number;
+  countReviews?: number;
+  translation: {
+    card: {
+      addToFollowing: string;
+      sale: string;
+      new: string;
+      available: string;
+      outOfStock: string;
+      buy: string;
     };
+  };
 }
 
-const TempComponent: React.FC<TempProps> = ({ product, locale, avgRating = 0, countReviews = 0, translation }) => {
-    const onClick = (id: number) => { console.log('productId', id) }
-    const onFClick = (id: number, isFavorite: boolean) => { console.log('productId from favorite', id, isFavorite) }
+const TempComponent: React.FC<TempProps> = ({
+  product,
+  locale,
+  avgRating = 0,
+  countReviews = 0,
+  translation,
+}) => {
+  const onClick = (id: number) => {
+    console.log('productId', id);
+  };
+  const onFClick = (id: number, isFavorite: boolean) => {
+    console.log('productId from favorite', id, isFavorite);
+  };
 
-    return <>
-        <Card product={product} locale={locale} avgRating={avgRating} isLogged={true} translation={translation} countReviews={countReviews} onBuyClick={onClick} onFavoriteClick={onFClick} variant='big' />
-    </>
-}
-export default TempComponent
+  return (
+    <Card
+      product={product}
+      locale={locale}
+      avgRating={avgRating}
+      isLogged={true}
+      translation={translation}
+      countReviews={countReviews}
+      onBuyClick={onClick}
+      onFavoriteClick={onFClick}
+      variant="big"
+    />
+  );
+};
+export default TempComponent;
